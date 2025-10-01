427c441fc25fad428c785a333529c5e9df994529
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-01 23:04:12 +0200

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  role: { type: String, default: 'user' } // 'admin' or 'user'
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
