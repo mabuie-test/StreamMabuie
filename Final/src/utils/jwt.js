1568a382dae435352aab941d312e3f328025196d
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-03 09:42:37 +0200

    Delete Final/src/utils/jwt.js

--- Final/src/utils/jwt.js
+++
@@ -1,4 +0,0 @@
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.JWT_SECRET || 'dev_secret';
-exports.sign = (payload, opts={}) => jwt.sign(payload, SECRET, Object.assign({ expiresIn: '7d' }, opts));
-exports.verify = (token) => jwt.verify(token, SECRET);
