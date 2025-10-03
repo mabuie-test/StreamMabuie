e07af4a0c533f2cd360576e2626d5c4f6d2906e4
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-03 09:45:24 +0200

    Delete Final/src/models/User.js

--- Final/src/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  role: { type: String, default: 'user' }
-}, { timestamps: true });
-module.exports = mongoose.model('User', UserSchema);
