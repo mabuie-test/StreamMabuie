eb16b22dcf8c6f88d4ba0ce224be5f6be9b74ba8
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-03 09:38:46 +0200

    Delete src directory

--- src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const apiCtrl = require('../controllers/apiController');
-
-router.get('/devices', apiCtrl.devices); // list devices (latest known)
-router.get('/device/:id/info', apiCtrl.deviceInfo);
-
-module.exports = router;
