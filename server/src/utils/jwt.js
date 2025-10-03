83b7ffa7526271c22655c7f91ec3da753b701e47
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-03 09:39:09 +0200

    Delete server directory

--- server/src/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const SECRET = process.env.JWT_SECRET || 'dev_secret';
-
-exports.sign = (payload, opts={}) => {
-  // ensure small payload
-  const p = Object.assign({}, payload);
-  return jwt.sign(p, SECRET, Object.assign({ expiresIn: '7d' }, opts));
-};
-exports.verify = (token) => jwt.verify(token, SECRET);
