7728c8eb23c3a6c75f8a57b4f929c51ee74106f4
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-03 07:47:21 +0200

    Add files via upload

---
+++ Final/src/utils/jwt.js
@@ -0,0 +1,4 @@
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.JWT_SECRET || 'dev_secret';
+exports.sign = (payload, opts={}) => jwt.sign(payload, SECRET, Object.assign({ expiresIn: '7d' }, opts));
+exports.verify = (token) => jwt.verify(token, SECRET);
